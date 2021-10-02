41232af198a830d6b1b43be8120472f75231ab23
Ethereal-bang <939847757@qq.com>
2021-10-02 09:21:02 +0800

    Student: "Application、Members"

--- src/pages/student/components/student_normal/component/ceo/ceo.js
+++ src/pages/student/components/student_normal/component/ceo/ceo.js
@@ -2,31 +2,6 @@ import React from 'react';
 import { Button, InputNumber, Table } from 'antd';
 import { showCeoVoter, applyCeo, voteCeo } from '../../../../../../pages/student/api/studentApi';
 
-const columns = [
-    {
-        title: '姓名',
-        dataIndex: 'userName',
-        key: 'userName',
-    },
-    {
-        title: '票数',
-        dataIndex: 'count',
-        key: 'count',
-        sorter: {
-            compare: (a, b) => a.count - b.count,
-            multiple: 1,
-        }
-    },
-    {
-        title: 'Action',
-        key: 'vote',
-        render: (_, record) => (
-            // eslint-disable-next-line
-            <a onClick={() => voteCeo(record.userName)}>为 {record.userName} 投票</a>
-        )
-    }
-];
-
 export class Ceo extends React.Component {
     constructor(props) {
         super(props);
@@ -50,7 +25,72 @@ export class Ceo extends React.Component {
 
     render() {
         const { voter } = this.state;
-        
+        const columns = [
+            {
+                title: '姓名',
+                dataIndex: 'userName',
+                key: 'userName',
+            },
+            {
+                title: '票数',
+                dataIndex: 'count',
+                key: 'count',
+                sorter: {
+                    compare: (a, b) => a.count - b.count,
+                    multiple: 1,
+                }
+            },
+            {
+                title: 'Action',
+                key: 'vote',
+                render: (_, record) => (
+                    // eslint-disable-next-line
+                    <a onClick={() => voteCeo(record.userName)}>为 {record.userName} 投票</a>
+                )
+            }
+        ];
+
+        const mockVoter = [
+                {
+                    "id": 2,
+                    "userId": "2011210012",
+                    "userName": "测试",
+                    "academy": null,
+                    "clazz": null,
+                    "discipline": null,
+                    "teacherClass": "SJ00201A2031780001",
+                    "score": 0.0,
+                    "companyId": "2",
+                    "position": null,
+                    "password": null,
+                    "count": "0",
+                    "voteList": null,
+                    "ceoVote": true,
+                    "hired": true,
+                    "ceoVoted": false,
+                    "companyVoted": false
+                },
+                {
+                    "id": 3,
+                    "userId": "2022222222",
+                    "userName": "测试2",
+                    "academy": null,
+                    "clazz": null,
+                    "discipline": null,
+                    "teacherClass": "SJ00201A2031780001",
+                    "score": 0.0,
+                    "companyId": "2",
+                    "position": null,
+                    "password": null,
+                    "count": "0",
+                    "voteList": null,
+                    "ceoVote": true,
+                    "hired": true,
+                    "ceoVoted": false,
+                    "companyVoted": false
+                }
+            ]
+
         return (
             <div className='site-page-header-ghost-wrapper'>
                 <Button type="primary" onClick={() => applyCeo()} style={{marginBottom: 20}}>
@@ -58,7 +98,7 @@ export class Ceo extends React.Component {
                 </Button>
                     <Table 
                         columns={columns} 
-                        dataSource={voter}
+                        dataSource={mockVoter}
                         bordered
                     />
             </div>
